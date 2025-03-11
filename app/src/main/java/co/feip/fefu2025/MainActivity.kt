78ed9eb0ec1651b12b2723e6803584c23eafdcab
Mikhail <mihailohrimenko300@gmail.com>
2025-03-11 11:37:48 +1000

    second point
    Added TextView in main activity with title and counter with default value 0. Counter value saves.

--- app/src/main/java/co/feip/fefu2025/MainActivity.kt
+++ app/src/main/java/co/feip/fefu2025/MainActivity.kt
@@ -1,6 +1,7 @@
 package co.feip.fefu2025
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,34 +15,26 @@ import androidx.compose.ui.tooling.preview.Preview
 import co.feip.fefu2025.ui.theme.FEFU2025AndroidBaseRepoTheme
 
 class MainActivity : ComponentActivity() {
+    private var counter: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            FEFU2025AndroidBaseRepoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "FEIP",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState != null)
+            counter = savedInstanceState.getInt("counter_value", 0)
+
+        val tapable_text: TextView = findViewById(R.id.title)
+        val counter_text: TextView = findViewById(R.id.counter)
+        counter_text.text = counter.toString()
+
+        tapable_text.setOnClickListener{
+            counter++
+            counter_text.text = counter.toString()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FEFU2025AndroidBaseRepoTheme {
-        Greeting("Android")
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("counter_value", counter)
     }
 }
